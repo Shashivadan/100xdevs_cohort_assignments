38631961d0afa315903427bead31bfc6d9df8ce8
Shashivadan <shashivadan99@gmail.com>
2023-12-08 00:07:20 +0530

    commited

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -4,9 +4,32 @@
   Transaction - an object like { itemName, category, price, timestamp }.
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
-
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+  let categorylist = {}
+
+  let arr = []
+
+  if(!transactions.length){
+    return []
+  }
+
+  transactions.forEach(transaction => {
+    const {category,price}= transaction
+
+    if(!categorylist.hasOwnProperty(category)){
+      categorylist[category] = 0
+    }
+    categorylist[category] += price;
+  });
+
+  const result = Object.entries(categorylist).map(([category, totalAmount]) => ({
+    "category":category,
+    totalSpent : totalAmount,
+  }));
+  
+
+  return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
