4cd481ffe566ba58fbb845dec65ee72f7de2a7b0
Shashivadan <shashivadan99@gmail.com>
2024-04-06 21:41:26 +0530

    changes: week-11 prisma solutions

--- week-10/2-prisma-simple/src/db/user.ts
+++ week-10/2-prisma-simple/src/db/user.ts
@@ -1,7 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-
 
 /*
  * Should insert into the users table
@@ -12,8 +11,25 @@ const prisma = new PrismaClient();
  *   name: string
  * }
  */
-export async function createUser(username: string, password: string, name: string) {
-    
+export async function createUser(
+  username: string,
+  password: string,
+  name: string
+) {
+  const res = await prisma.user.create({
+    data: {
+      username,
+      password,
+      name,
+    },
+    select: {
+      id: true,
+      username: true,
+      password: true,
+      name: true,
+    },
+  });
+  return res;
 }
 
 /*
@@ -25,5 +41,16 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+  const res = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      id: true,
+      username: true,
+      password: true,
+      name: true,
+    },
+  });
+  return res;
 }
